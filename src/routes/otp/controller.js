80ba6db1b704389788b051360595c2f42923f3e5
Abhishek Gupta <hi@itsabhishek.me>
2020-03-27 16:02:57 +0530

    errors should be sent with correct http code

--- src/routes/otp/controller.js
+++ src/routes/otp/controller.js
@@ -32,7 +32,7 @@ module.exports.handleSendOtp = async (req, res, next) => {
     } catch (err) {
       sentry.captureException(err)
       await revert()
-      return res.json(new ResponseError('SMS_API_ERROR', "Can't send OTP to that number"))
+      return res.status(400).json(new ResponseError('SMS_API_ERROR', "Can't send OTP to that number"))
     }
 
   } else if (req.body.email) {
@@ -59,7 +59,7 @@ module.exports.handleSendOtp = async (req, res, next) => {
      sentry.captureException(err)
      // revert DB insert
      await revert()
-     return res.json(new ResponseError('EMAIL_API_ERROR', "Can't send OTP to that email"))
+     return res.status(400).json(new ResponseError('EMAIL_API_ERROR', "Can't send OTP to that email"))
    }
 
   } else {
