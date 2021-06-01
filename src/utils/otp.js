fe94054353d6567912a99d0f5b970fe3302329ec
Vibhu Dujari <vdvibhu20@gmail.com>
2021-06-01 22:45:18 +0530

    update: default sms template

--- src/utils/otp.js
+++ src/utils/otp.js
@@ -1,6 +1,6 @@
 const Mustache = require('mustache')
 module.exports.generateOtp = () => Math.floor(100000 + Math.random() * 900000); //creates a 6 digit random number.
 
-module.exports.getOtpMessageText = function (otp, template = 'Your OTP is: {{code}}') {
+module.exports.getOtpMessageText = function (otp, template = `{{code}} is the OTP for Mobile Verification valid for 10 mins. Do not share it with anyone. \n-Coding Blocks`) {
   return Mustache.render(template, { code: otp })
 }
