cd2bdd8fc6c1e6945076fd3876671698de5f76f7
Vibhu Dujari <vdvibhu20@gmail.com>
2021-06-03 14:16:51 +0530

    fix: Joi validations send error in res

--- src/routes/otp/validator.js
+++ src/routes/otp/validator.js
@@ -1,8 +1,11 @@
 const Joi = require('@hapi/joi')
 
 const POST = function (req, res, next) {
-  const schema = Joi.object({
+  const schema = Joi.object().keys({
     mobile: Joi.string().length(10),
+    msgTemplate: Joi.string()
+                    .valid('MOBILE_VERIFICATION','ACCOUNT_ACCESS')
+                    .when('mobile', { is: Joi.exist(), then: Joi.required()}),
     email: Joi.string().email(),
     template: Joi.string(),
     dialCode: Joi.string(),
@@ -10,7 +13,10 @@ const POST = function (req, res, next) {
   })
 
   try {
-    schema.validate(req.body)
+    const { error } = schema.validate(req.body)
+    if (error) {
+      return res.status(400).json({message: error.details[0].message})
+    }
     next()
   } catch (err) {
     res.status(400).json(err)
