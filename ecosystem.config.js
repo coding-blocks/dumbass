be1504ba8b8eb5f9ed1b382f72d75666d1319332
Sparsh <sparshgoelk@gmail.com>
2026-05-04 17:24:43 +0530

    fixed env in ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,23 @@
+require('dotenv').config({ path: require('path').resolve(__dirname, '.env') })
+
 module.exports = {
   apps: [
     {
       name: 'email-service',
       script: 'src/index.js',
-      node_args: '--require dotenv/config',
       env: {
         NODE_PATH: 'src',
-        PORT: 5001,
+        PORT: process.env.PORT || 5001,
+        MONGODB_URI: process.env.MONGODB_URI,
+        DATABASE_NAME: process.env.DATABASE_NAME,
+        SENDGRID_KEY: process.env.SENDGRID_KEY,
+        SENDGRID_SENDER_EMAIL: process.env.SENDGRID_SENDER_EMAIL,
+        SENDGRID_SENDER_NAME: process.env.SENDGRID_SENDER_NAME,
+        SENDGRID_TEMPLATE_ID: process.env.SENDGRID_TEMPLATE_ID,
+        TWO_FACTOR_ENDPOINT: process.env.TWO_FACTOR_ENDPOINT,
+        TWO_FACTOR_NAMESPACE: process.env.TWO_FACTOR_NAMESPACE,
+        TWO_FACTOR_SECRET: process.env.TWO_FACTOR_SECRET,
+        SENTRY_DSN: process.env.SENTRY_DSN,
       },
       instances: 1,
       autorestart: true,
